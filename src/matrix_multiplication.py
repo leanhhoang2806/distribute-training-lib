2327ba875f4782712937c4e0e851af7acdc30730
chikki-admin <chikkiaquatics@gmail.com>
2023-08-02 01:22:32 -0500

    test

--- src/matrix_multiplication.py
+++ src/matrix_multiplication.py
@@ -23,14 +23,21 @@ def matrix_multiplication(matrix1, matrix2):
     return result
 
 
-@cuda.jit
-def matrix_multiplication_kernel(matrix1, matrix2, result, rows1, cols1, cols2):
+@cuda.jit(target='cuda', device=True)
+def matrix_multiplication_kernel(matrix1, matrix2, rows1, cols1, cols2):
     i, j = cuda.grid(2)
     if i < rows1 and j < cols2:
         tmp = 0.0
         for k in range(cols1):
             tmp += matrix1[i, k] * matrix2[k, j]
-        result[i, j] = tmp
+        return tmp
+    return 0.0
+
+@cuda.jit
+def matrix_multiplication(matrix1, matrix2, result):
+    i, j = cuda.grid(2)
+    if i < result.shape[0] and j < result.shape[1]:
+        result[i, j] = matrix_multiplication_kernel(matrix1, matrix2, result.shape[0], matrix1.shape[1], matrix2.shape[1])
 
 def matrix_multiplication_cuda(matrix1, matrix2):
     if count_gpus() == 0:
@@ -53,10 +60,9 @@ def matrix_multiplication_cuda(matrix1, matrix2):
     grid_size = ((cols2 - 1) // block_size[0] + 1, (rows1 - 1) // block_size[1] + 1)
 
     # Launch the CUDA kernel
-    matrix_multiplication_kernel[grid_size, block_size](matrix1_gpu, matrix2_gpu, result_gpu, rows1, cols1, cols2)
+    matrix_multiplication[grid_size, block_size](matrix1_gpu, matrix2_gpu, result_gpu)
 
     # Move the result back to CPU memory
     result_cpu = result_gpu.copy_to_host()
 
     return result_cpu.tolist()  # Convert NumPy array back to a Python list
-
