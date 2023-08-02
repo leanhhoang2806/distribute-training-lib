9a3ddf33cc007ca5c92abd64033759ac98e191c6
chikki-admin <chikkiaquatics@gmail.com>
2023-08-02 01:17:10 -0500

    change to numba

--- src/matrix_multiplication.py
+++ src/matrix_multiplication.py
@@ -1,4 +1,5 @@
-import cupy as cp
+
+from numba import cuda
 import numpy as np
 from src.gpu_availability import count_gpus
 
@@ -21,6 +22,16 @@ def matrix_multiplication(matrix1, matrix2):
 
     return result
 
+
+@cuda.jit
+def matrix_multiplication_kernel(matrix1, matrix2, result, rows1, cols1, cols2):
+    i, j = cuda.grid(2)
+    if i < rows1 and j < cols2:
+        tmp = 0.0
+        for k in range(cols1):
+            tmp += matrix1[i, k] * matrix2[k, j]
+        result[i, j] = tmp
+
 def matrix_multiplication_cuda(matrix1, matrix2):
     if count_gpus() == 0:
         raise Exception("No GPUs found on this computer.")
@@ -31,26 +42,21 @@ def matrix_multiplication_cuda(matrix1, matrix2):
         raise ValueError("Number of columns in matrix1 must be equal to the number of rows in matrix2")
 
     # Move matrices to GPU memory
-    matrix1_gpu = cp.asarray(matrix1, dtype=np.float32)
-    matrix2_gpu = cp.asarray(matrix2, dtype=np.float32)
+    matrix1_gpu = cuda.to_device(np.array(matrix1, dtype=np.float32))
+    matrix2_gpu = cuda.to_device(np.array(matrix2, dtype=np.float32))
 
     # Allocate memory for the result on GPU
-    result_gpu = cp.empty((rows1, cols2), dtype=np.float32)
+    result_gpu = cuda.device_array((rows1, cols2), dtype=np.float32)
 
     # Define block and grid sizes for CUDA kernel
     block_size = (32, 32)
     grid_size = ((cols2 - 1) // block_size[0] + 1, (rows1 - 1) // block_size[1] + 1)
 
-    # Load and compile the CUDA kernel
-    with open('matrix_mult.cu', 'r') as f:
-        kernel_code = f.read()
-
-    matrix_mult_kernel = cp.RawKernel(kernel_code, 'matrix_multiplication')
-
     # Launch the CUDA kernel
-    matrix_mult_kernel(grid_size, block_size, (matrix1_gpu, matrix2_gpu, result_gpu, rows1, cols1, cols2))
+    matrix_multiplication_kernel[grid_size, block_size](matrix1_gpu, matrix2_gpu, result_gpu, rows1, cols1, cols2)
 
     # Move the result back to CPU memory
-    result_cpu = cp.asnumpy(result_gpu)
+    result_cpu = result_gpu.copy_to_host()
 
-    return result_cpu
+    return result_cpu.tolist()  # Convert NumPy array back to a Python list
+
