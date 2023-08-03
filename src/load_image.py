31d0a010391b686b10436f97d6a2a3343276d115
chikki-admin <chikkiaquatics@gmail.com>
2023-08-02 23:08:11 -0500

    reshape then transpose

--- src/load_image.py
+++ src/load_image.py
@@ -10,7 +10,7 @@ def load_images_from_folder(folder_path):
             image_pil = Image.open(image_path)
             image_np = np.array(image_pil)
             # flatten the image
-            image_np = image_np.reshape(-1)
+            image_np = image_np.reshape(-1, 1)
             
-            image_list.append(np.transpose(image_np))
+            image_list.append(image_np.T)
     return image_list
