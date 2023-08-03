c7060c4a9d6a5291e92c2ade8627baf3a0dec05f
chikki-admin <chikkiaquatics@gmail.com>
2023-08-03 00:35:55 -0500

    generate random matrix

--- src/image_matrix_interface.py
+++ src/image_matrix_interface.py
@@ -4,5 +4,5 @@ import numpy as np
 
 def process_image_engine(folder_path):
     loaded_images = load_images_from_folder(folder_path)
-    random_array = np.random.rand(*loaded_images)
+    random_array = np.random.rand(loaded_images.shape[0], loaded_images.shape[1])
     return matrix_multiplication_cuda(loaded_images, random_array)
