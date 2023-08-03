956889432aba268ad4289f45214892dfaf5475ce
chikki-admin <chikkiaquatics@gmail.com>
2023-08-02 22:57:37 -0500

    flatten

--- src/load_image.py
+++ src/load_image.py
@@ -9,5 +9,7 @@ def load_images_from_folder(folder_path):
             image_path = os.path.join(folder_path, filename)
             image_pil = Image.open(image_path)
             image_np = np.array(image_pil)
+            # flatten the image
+            image_np = image_np.reshape(-1)
             image_list.append(image_np)
     return image_list
