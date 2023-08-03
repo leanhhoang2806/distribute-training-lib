c9cb1dad4fbeda8a8582c28e9271ca8f6b27289d
chikki-admin <chikkiaquatics@gmail.com>
2023-08-02 20:40:08 -0500

    pycuda

--- src/matrix_multiplication.py
+++ src/matrix_multiplication.py
@@ -1,7 +1,47 @@
 
-from numba import cuda
 import numpy as np
 from src.gpu_availability import count_gpus
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+import os
+
+def matrix_multiplication_cuda(A, B):
+    rows_A, cols_A = A.shape
+    rows_B, cols_B = B.shape
+
+    if cols_A != rows_B:
+        raise ValueError("Matrix dimensions are not compatible for multiplication.")
+
+    # Allocate GPU memory
+    A_gpu = cuda.mem_alloc(A.nbytes)
+    B_gpu = cuda.mem_alloc(B.nbytes)
+    C_gpu = cuda.mem_alloc((rows_A * cols_B * np.dtype(np.float32).itemsize))
+
+    # Transfer data to GPU
+    cuda.memcpy_htod(A_gpu, A)
+    cuda.memcpy_htod(B_gpu, B)
+
+    # Define block and grid sizes
+    block_size = (16, 16, 1)
+    grid_size = ((cols_B - 1) // block_size[0] + 1, (rows_A - 1) // block_size[1] + 1)
+    print("Block size is " + str(block_size))
+    print("Grid size is " + str(grid_size))
+
+    # Compile CUDA kernel
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    cuda_file = os.path.join(current_dir, "matrix_mult.cu")
+    mod = SourceModule(open(cuda_file).read())
+    matrix_multiply = mod.get_function("matrix_multiply")
+
+    # Call the CUDA kernel
+    matrix_multiply(A_gpu, B_gpu, C_gpu, np.int32(rows_A), np.int32(cols_A), np.int32(cols_B), block=block_size, grid=grid_size)
+
+    # Allocate memory for the result on the host and transfer the result from the GPU
+    C = np.empty((rows_A, cols_B), dtype=np.float32)
+    cuda.memcpy_dtoh(C, C_gpu)
+
+    return C
 
 
 def matrix_multiplication(matrix1, matrix2):
@@ -21,41 +61,3 @@ def matrix_multiplication(matrix1, matrix2):
                 result[i][j] += matrix1[i][k] * matrix2[k][j]
 
     return result
-
-
-@cuda.jit
-def matrix_multiplication(matrix1, matrix2, result):
-    i, j = cuda.grid(2)
-    if i < result.shape[0] and j < result.shape[1]:
-        tmp = 0.0
-        for k in range(matrix1.shape[1]):
-            tmp += matrix1[i, k] * matrix2[k, j]
-        result[i, j] = tmp
-
-def matrix_multiplication_cuda(matrix1, matrix2):
-    if count_gpus() == 0:
-        raise Exception("No GPUs found on this computer.")
-    rows1, cols1 = len(matrix1), len(matrix1[0])
-    rows2, cols2 = len(matrix2), len(matrix2[0])
-
-    if cols1 != rows2:
-        raise ValueError("Number of columns in matrix1 must be equal to the number of rows in matrix2")
-
-    # Move matrices to GPU memory
-    matrix1_gpu = cuda.to_device(np.array(matrix1, dtype=np.float32))
-    matrix2_gpu = cuda.to_device(np.array(matrix2, dtype=np.float32))
-
-    # Allocate memory for the result on GPU
-    result_gpu = cuda.device_array((rows1, cols2), dtype=np.float32)
-
-    # Define block and grid sizes for CUDA kernel
-    block_size = (32, 32)
-    grid_size = ((rows1 - 1) // block_size[0] + 1, (cols2 - 1) // block_size[1] + 1)
-
-    # Launch the CUDA kernel
-    matrix_multiplication[grid_size, block_size](matrix1_gpu, matrix2_gpu, result_gpu)
-
-    # Move the result back to CPU memory
-    result_cpu = result_gpu.copy_to_host()
-
-    return result_cpu.tolist()  # Convert NumPy array back to a Python list
