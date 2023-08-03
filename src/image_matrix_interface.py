9e8c5df90cad8550246023e7771f316c521c7a9c
chikki-admin <chikkiaquatics@gmail.com>
2023-08-02 22:36:12 -0500

    1-d array

--- src/image_matrix_interface.py
+++ src/image_matrix_interface.py
@@ -7,7 +7,7 @@ def process_image_engine(folder_path):
     caculation_results = []
     for image_np_array in loaded_images:
         # generate a random np array with the same size as the image
-        random_array = np.random.rand(image_np_array.shape[0], image_np_array.shape[1])
+        random_array = np.random.rand(image_np_array.shape[0])
         print(f"image_np_array: {image_np_array}")
         caculation_results.append(matrix_multiplication_cuda(image_np_array, random_array))
     print("caculation_results: ", caculation_results)
