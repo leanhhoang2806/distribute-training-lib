9354cef7a15be3d5894f020c0f16d273ce3af3f5
chikki-admin <chikkiaquatics@gmail.com>
2023-08-02 22:43:08 -0500

    correct method

--- src/image_matrix_interface.py
+++ src/image_matrix_interface.py
@@ -7,8 +7,9 @@ def process_image_engine(folder_path):
     caculation_results = []
     for image_np_array in loaded_images:
         # generate a random np array with the same size as the image
-        random_array = np.random.rand(image_np_array.shape[0])
-        print(f"image_np_array: {image_np_array}")
+        random_array = np.random.rand(len(image_np_array))
+        print(f"image_np_array: {image_np_array.shape}")
+        print(f"random_array: {random_array.shape}")
         caculation_results.append(matrix_multiplication_cuda(image_np_array, random_array))
     print("caculation_results: ", caculation_results)
     return caculation_results
