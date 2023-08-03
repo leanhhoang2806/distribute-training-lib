b9e5094c933321874b105d64d3698e388e0cafff
chikki-admin <chikkiaquatics@gmail.com>
2023-08-02 21:13:08 -0500

    resource warning

--- tests/test_matrix_multiplcation.py
+++ tests/test_matrix_multiplcation.py
@@ -1,8 +1,16 @@
 import unittest
 import numpy as np
+import tracemalloc
 from src.matrix_multiplication import matrix_multiplication_cuda
 
 class TestMatrixMultiplication(unittest.TestCase):
+    def setUp(self):
+        # Enable tracemalloc before each test
+        tracemalloc.start()
+
+    def tearDown(self):
+        # Stop tracemalloc after each test
+        tracemalloc.stop()
     def test_matrix_multiplication(self):
         A = np.array([[1, 2], [3, 4], [5, 6]], dtype=np.float32)
         B = np.array([[7, 8, 9], [10, 11, 12]], dtype=np.float32)
