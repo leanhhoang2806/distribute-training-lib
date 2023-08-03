9c96f7f3adc0d8496325d211e4632b8e8f7dfaec
chikki-admin <chikkiaquatics@gmail.com>
2023-08-03 00:42:42 -0500

    rework random array

--- src/image_matrix_interface.py
+++ src/image_matrix_interface.py
@@ -4,5 +4,7 @@ import numpy as np
 
 def process_image_engine(folder_path):
     loaded_images = load_images_from_folder(folder_path)
-    random_array = np.random.rand(loaded_images.shape[0], loaded_images.shape[1])
+    print("Loaded images: {}".format(loaded_images.shape))
+    random_array = np.random.rand(loaded_images.shape[1], loaded_images.shape[0])
+    print("Random array: {}".format(random_array.shape))
     return matrix_multiplication_cuda(loaded_images, random_array)
